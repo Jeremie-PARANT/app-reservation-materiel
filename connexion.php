56153739baa100844d41d986d511a40ade886089
RubenDavidAbreu <rubendavid954@gmail.com>
2023-04-17 13:56:03 +0200

    modif code

--- connexion.php
+++ connexion.php
@@ -11,17 +11,20 @@
 <?php 
 include_once('includes/header_authentication.php'); 
 ?>
-    <form method="post">
+    <form action="connexion.php" method="post">
         <div class="form_block">
-       <div class="form_txt">Mail :</div><input type="text">
-       <div class="form_txt">Mot de passe :</div><input type="text">
+       <div class="form_txt">Mail :</div>   <input type="text" name="mail">
+       <div class="form_txt">Mot de passe :</div>   <input type="text" name="mdp">
         </div>
         <input type="submit" value="Se connecter">
     </form>
 
     <?php
-
-    
+    if (empty($_POST['mail']) && empty($_POST['mail'])) {
+    $mail = $_POST ['mail'];
+    $mpd = $_POST ['mdp'];
+    echo "t une merde";
+    }
     ?>
 
 </body>
