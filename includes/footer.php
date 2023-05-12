3a05591c7868cd254774e7b73223c5c38c069be5
lenormandmorgane <morg.l.n1001@gmail.com>
2023-05-12 11:07:54 +0200

    header footer

---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+<style>
+footer {
+    position: absolute ;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    height: 100px;
+    background-color: #2E3494;
+}
+.img2 {
+    padding-left: 1325px;
+    padding-top: 15px;
+    width :200px;
+    height : 70px;
+}
+a {
+    text-decoration: None;
+}
+
+
+</style>
+<footer>
+    <img src="SAE_203/images/logoIUT.jpg" class="img2"/>
+</footer>
